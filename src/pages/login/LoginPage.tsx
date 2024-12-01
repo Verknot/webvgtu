efe0a057557e650aac64a6f8615b3bc03e9d7e90
Александр Дудников <dudnikov@hd-tech.team>
2024-12-01 19:48:02 +0300

    Задание 5.

--- src/pages/login/LoginPage.tsx
+++ src/pages/login/LoginPage.tsx
@@ -1,5 +1,7 @@
 import {FC, useState} from "react";
 import {Button, TextField} from "../../components";
+import {WidgetLayout} from "../../components/layouts";
+import "./loginPageStyle.scss"
 
 export const LoginPage: FC = () => {
     const [login, setLogin] = useState<string>("")
@@ -13,7 +15,14 @@ export const LoginPage: FC = () => {
         setPassword(value);
     }
 
-    const LoginHandler = () => {
+    const loginHandler = () => {
+        console.log({
+            login,
+            password
+        })
+    }
+
+    const toRegistrationHandler = () => {
         console.log({
             login,
             password
@@ -21,10 +30,19 @@ export const LoginPage: FC = () => {
     }
 
     return (
-        <>
-            <TextField labelText="Логин" value={login} type="text" onChange={loginChangedHandler} />
-            <TextField labelText="Пароль" value={password} type="password" onChange={passwordChangedHandler} />
-            <Button text = "Войти" onClick={LoginHandler}/>
-        </>
+        <WidgetLayout>
+            <div className="login-page__form">
+                <div className="login-page__title">Вход</div>
+                <div className="login-page__fields">
+                    <TextField labelText="Логин" value={login} type="text" onChange={loginChangedHandler} />
+                    <TextField labelText="Пароль" value={password} type="password" onChange={passwordChangedHandler} />
+                </div>
+                <div className="login-page__actions">
+                    <Button text = "Войти" onClick={loginHandler} type="primary"/>
+                    <Button text = "Зарегистрироваться" onClick={toRegistrationHandler} type="secondary"/>
+                </div>
+
+            </div>
+        </WidgetLayout>
     )
 }
