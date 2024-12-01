f9055cdc80d2d8b3f49160130d6d855defbb0a2d
Александр Дудников <dudnikov@hd-tech.team>
2024-12-01 16:09:21 +0300

    Задание 2.

---
+++ src/pages/login/LoginPage.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+import {TextField} from "../../components/textField";
+
+export const LoginPage: FC = () => {
+
+    return (
+        <>
+            <TextField labelText="Login LAbel" value={"dasdadas"}/>
+        </>
+    )
+}
