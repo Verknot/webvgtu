698a1a72e41f36490cd0f9203c2323497d85a270
Александр Дудников <dudnikov@hd-tech.team>
2024-12-01 18:38:51 +0300

    Задание 4.

---
+++ src/components/button/Button.tsx
@@ -0,0 +1,15 @@
+import {FC} from "react";
+import {ButtonProps} from "./ButtonProps";
+
+export const Button: FC<ButtonProps> = props => {
+    const {
+        onClick,
+        text
+    } = props
+
+    return(
+        <div onClick={onClick} className={"button"}>
+            {text}
+        </div>
+    )
+}
--- src/pages/login/LoginPage.tsx
+++ src/pages/login/LoginPage.tsx
@@ -1,5 +1,5 @@
 import {FC, useState} from "react";
-import {TextField} from "../../components/textField";
+import {Button, TextField} from "../../components";
 
 export const LoginPage: FC = () => {
     const [login, setLogin] = useState<string>("")
@@ -13,10 +13,18 @@ export const LoginPage: FC = () => {
         setPassword(value);
     }
 
+    const LoginHandler = () => {
+        console.log({
+            login,
+            password
+        })
+    }
+
     return (
         <>
             <TextField labelText="Логин" value={login} type="text" onChange={loginChangedHandler} />
             <TextField labelText="Пароль" value={password} type="password" onChange={passwordChangedHandler} />
+            <Button text = "Войти" onClick={LoginHandler}/>
         </>
     )
 }
