cde25341ae1621838e3444fd3d6ba7e1b2750695
Александр Дудников <dudnikov@hd-tech.team>
2024-12-01 20:54:21 +0300

    Задание 7.

---
+++ src/pages/department/DepartmentsPage.tsx
@@ -0,0 +1,11 @@
+import {FC} from "react";
+import {Layout} from "../../components/layouts";
+
+export const DepartmentsPage: FC = () =>{
+
+    return (
+        <Layout footer={<>footer</>}>
+            <div>DepartmentsPage</div>
+        </Layout>
+    )
+}
