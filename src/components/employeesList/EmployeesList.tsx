e0837aefb1063a8aace8acc8c5d7e55bd804f91a
Александр Дудников <dudnikov@hd-tech.team>
2024-12-09 00:36:33 +0300

    Задание 12.

--- src/components/employeesList/EmployeesList.tsx
+++ src/components/employeesList/EmployeesList.tsx
@@ -5,12 +5,25 @@ import clsx from "classnames";
 import {PencilIcon, TrashIcon} from "../../assets/icons";
 
 export const EmployeesList: FC<EmployeesListProps> = props => {
-    const { employeesList, onItemClick } = props;
+    const {
+        employeesList,
+        onItemClick,
+        onItemDelete,
+        onItemEdit
+    } = props;
     const [selectedUser, setSelectedUser] = useState(0);
 
     const employeeClickHandler = (id: number) =>{
         setSelectedUser(id);
         onItemClick && onItemClick(id);
+    }
+
+    const employeeEditHandler = (id: number) => {
+        onItemEdit && onItemEdit(id);
+    }
+
+    const employeeDeleteHandler = (id: number) => {
+        onItemDelete && onItemDelete(id)
     }
 
     const isSelected = (id: number) => selectedUser === id;
@@ -21,11 +34,11 @@ export const EmployeesList: FC<EmployeesListProps> = props => {
                         className={clsx("empl-list__item", {"empl-list__item_selected" : isSelected(user.id)})}
                         onClick= {() => employeeClickHandler(user.id)} >
                 <div className="empl-list__item_fio">
-                    <span>{`${user.lastName} ${user.firstName} ${user.middleName ?? ''}`.trim()}</span>
+                    <span>{`${user.lastName} ${user.firstName} ${user.midleName ?? ''}`.trim()}</span>
                 </div>
                 <div className="empl-list__item_actions">
-                    <PencilIcon width={18} height={18}/>
-                    <TrashIcon width={18} height={18}/>
+                    <PencilIcon width={18} height={18} onClick={() => {employeeEditHandler(user.id)}} />
+                    <TrashIcon width={18} height={18} onClick={() => {employeeDeleteHandler(user.id)}} />
                 </div>
             </div>)
         })}
