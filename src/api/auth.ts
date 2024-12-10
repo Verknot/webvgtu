9bccd36a4d2a5fa2020a57643f1a4d74f7b53cc6
Александр Дудников <dudnikov@hd-tech.team>
2024-12-10 18:50:12 +0300

    Задание 15.

---
+++ src/api/auth.ts
@@ -0,0 +1,30 @@
+import { AxiosInstance} from "./axiosInstance"
+const {axiosPost} = AxiosInstance();
+
+interface LoginRequestDto {
+    login: string;
+    password: string;
+}
+
+interface LoginResponseDto {
+    access_token: string;
+    username: string;
+    role: string;
+}
+
+interface RegistrationRequestDto{
+    login: string;
+    password: string;
+}
+
+const signIn = async(loginData: LoginRequestDto) =>
+    await axiosPost('/login', loginData) as LoginResponseDto;
+
+
+const signUp = async(registrationData: RegistrationRequestDto)=>
+    await axiosPost('', registrationData) as void;
+
+export const Auth = {
+    signIn,
+    signUp
+}
